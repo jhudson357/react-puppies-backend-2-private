9582f1e8751c2d5f2fdb737fa4db9574019c48eb
James Hudson <jmh357@comcast.net>
2022-12-20 14:58:10 -0600

    fix error in show route. final commit

--- routes/puppies.js
+++ routes/puppies.js
@@ -4,7 +4,7 @@ import * as puppiesCtrl from '../controllers/puppies.js'
 const router = Router()
 
 router.get('/', puppiesCtrl.index)
-router.get(':id', puppiesCtrl.show)
+router.get('/:id', puppiesCtrl.show)
 router.post('/', puppiesCtrl.create)
 router.put('/:id', puppiesCtrl.update)
 router.delete('/:id', puppiesCtrl.delete)
