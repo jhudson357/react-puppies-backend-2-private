fca10528ba9ec6f239714a46dd84ce420ca8cda6
James Hudson <jmh357@comcast.net>
2022-12-20 16:15:37 -0600

    import cors and update port to 3001

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
+import cors from 'cors'
 import ('./config/database.js')
 
 // import routers
@@ -20,6 +21,7 @@ app.set(
 app.set('view engine', 'ejs')
 
 // middleware
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
